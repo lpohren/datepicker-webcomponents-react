edb96c5abcb4f3282f68eb1140d2b6b5c71e50b2
Luis Pohren <luis.pohren@sap.com>
2021-05-25 15:02:52 -0300

    datepicker adjust

--- DatePicker.js
+++ DatePicker.js
@@ -1,18 +1,22 @@
-import { ValueState } from '@ui5/webcomponents-react/dist/ValueState';
-import { withWebComponent } from '@ui5/webcomponents-react/dist/withWebComponent';
+import { CalendarType } from '@ui5/webcomponents-react/lib/CalendarType';
+import { ValueState } from '@ui5/webcomponents-react/lib/ValueState';
+import { withWebComponent } from '@ui5/webcomponents-react/lib/withWebComponent';
 import '@ui5/webcomponents/dist/DatePicker';
 
 /**
- * The `DatePicker` component provides an input field with assigned calendar which opens on user action. The `DatePicker` allows users to select a localized date using touch, mouse, or keyboard input. It consists of two parts: the date input field and the date picker.
- *
- * <ui5-link href="https://sap.github.io/ui5-webcomponents/playground/components/DatePicker" target="_blank">UI5 Web Components Playground</ui5-link>
+ * The <code>DatePicker</code> component provides an input field with assigned calendar which opens on user action. The
+ <code>DatePicker</code> allows users to select a localized date using touch, mouse, or keyboard input. It consists of
+ two parts: the date input field and the date picker.
+
+ * <a href="https://sap.github.io/ui5-webcomponents/playground/components/DatePicker" target="_blank">UI5 Web Components Playground</a>
  */
-var DatePicker = withWebComponent('ui5-date-picker', ['name', 'placeholder', 'value', 'valueState', 'formatPattern', 'maxDate', 'minDate', 'primaryCalendarType'], ['disabled', 'hideWeekNumbers', 'readonly', 'required'], ['valueStateMessage'], ['change', 'input']);
+var DatePicker = withWebComponent('ui5-date-picker', ['formatPattern', 'maxDate', 'minDate', 'name', 'placeholder', 'primaryCalendarType', 'value', 'valueState'], ['disabled', 'hideWeekNumbers', 'readonly', 'required'], ['valueStateMessage'], ['change', 'input']);
 DatePicker.displayName = 'DatePicker';
 DatePicker.defaultProps = {
   disabled: false,
   hideWeekNumbers: false,
   placeholder: undefined,
+  primaryCalendarType: CalendarType.Gregorian,
   readonly: false,
   required: false,
   valueState: ValueState.None
