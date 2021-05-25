817d333e8dfe39bd8bbacb1f927d2a76def376ad
Luis Pohren <luis.pohren@sap.com>
2021-05-25 00:14:54 -0300

    babel

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,6 @@
-{
-    "presets": [
+module.exports = {
+    presets:[
         "@babel/preset-env",
         "@babel/preset-react"
-    ],
-    "plugins": [
-        "@babel/plugin-transform-react-jsx",
     ]
 }
