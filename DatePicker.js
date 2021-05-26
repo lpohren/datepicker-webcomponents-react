4969ccb9701eeeaae034000ff41f9daa1264c258
Luis Pohren <luis.pohren@sap.com>
2021-05-26 15:22:55 -0300

    change tag

--- DatePicker.js
+++ DatePicker.js
@@ -1,20 +1,20 @@
-import { CalendarType } from '@ui5/webcomponents-react/lib/CalendarType';
-import { ValueState } from '@ui5/webcomponents-react/lib/ValueState';
-import { withWebComponent } from '@ui5/webcomponents-react/lib/withWebComponent';
+import { ValueState } from '@ui5/webcomponents-react/dist/ValueState';
+import { withWebComponent } from '@ui5/webcomponents-react/dist/withWebComponent';
 import '@ui5/webcomponents/dist/DatePicker';
 
 /**
- * <code>import { DatePicker } from '@ui5/webcomponents-react/lib/DatePicker';</code>
- * <br />
- * <a href="https://sap.github.io/ui5-webcomponents/playground/components/DatePicker" target="_blank">UI5 Web Components Playground</a>
+ * The `DatePicker` component provides an input field with assigned calendar which opens on user action. The `DatePicker` allows users to select a localized date using touch, mouse, or keyboard input. It consists of two parts: the date input field and the date picker.
+ *
+ * <ui5-link href="https://sap.github.io/ui5-webcomponents/playground/components/DatePicker" target="_blank">UI5 Web Components Playground</ui5-link>
  */
-var DatePicker = withWebComponent('ui5-datepicker', ['formatPattern', 'maxDate', 'minDate', 'name', 'placeholder', 'primaryCalendarType', 'value', 'valueState'], ['disabled', 'readonly'], [], ['change', 'input']);
+var DatePicker = withWebComponent('ui5-date-picker-neeew', ['name', 'placeholder', 'value', 'valueState', 'formatPattern', 'maxDate', 'minDate', 'primaryCalendarType'], ['disabled', 'hideWeekNumbers', 'readonly', 'required'], ['valueStateMessage'], ['change', 'input']);
 DatePicker.displayName = 'DatePicker';
 DatePicker.defaultProps = {
   disabled: false,
+  hideWeekNumbers: false,
   placeholder: undefined,
-  primaryCalendarType: CalendarType.Gregorian,
   readonly: false,
+  required: false,
   valueState: ValueState.None
 };
 
